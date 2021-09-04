c23eba81c9b79be5b611b3a66a60e185d3176622
Siddhant Doshi <66712952+siddhant-doshi@users.noreply.github.com>
2021-09-04 12:32:09 +0530

    Update datasets.py

--- datasets.py
+++ datasets.py
@@ -8,6 +8,7 @@ from scipy.sparse import coo_matrix
 import torch
 from ogb.graphproppred import DglGraphPropPredDataset, collate_dgl
 from torch.utils.data import DataLoader
+import re
 
 def get_sparse_tensor_from_scipy_coo(coo):
   values = coo.data
@@ -71,11 +72,28 @@ Reddit_M = {
     "input_feat_dim":1,
     "num_classes":5
 }
-
 ogbg_molhiv = {
     "name":"ogbg-molhiv",
     "category":"ogb",
     "input_feat_dim":9,
+    "num_classes":2
+}
+KKI = {
+    "name":"KKI",
+    "category":"brain",
+    "input_feat_dim":1,
+    "num_classes":2
+}
+OHSU = {
+    "name":"OHSU",
+    "category":"brain",
+    "input_feat_dim":1,
+    "num_classes":2
+}
+Peking_1 = {
+    "name":"Peking_1",
+    "category":"brain",
+    "input_feat_dim":1,
     "num_classes":2
 }
 
@@ -89,7 +107,10 @@ dataset_dict = {
     "COLLAB":Collab,
     "REDDIT-BINARY":Reddit_B,
     "REDDIT-MULTI-5K":Reddit_M,
-    "ogbg-molhiv":ogbg_molhiv
+    "ogbg-molhiv":ogbg_molhiv,
+    "KKI":KKI,
+    "OHSU":OHSU,
+    "Peking_1":Peking_1
 }
 
 class dataset():
@@ -98,18 +119,21 @@ class dataset():
     self.name = name
     self.category = category
     self.num_classes = num_classes
-    if (self.category == 'ogb'):
+    if (self.category == 'brain'):
+      self.graphs, self.labels = construct_dataset(predefined_splits_filepath)
+      self.input_feat_dim = 1 #using the degree information
+      self.stratification_ratios = list(self.get_stratification_ratios(self.labels).values())
+    elif (self.category == 'ogb'):
       self.data = DglGraphPropPredDataset(name = name)
       self.graphs = [x[0] for x in self.data]
       self.labels = [x[1].item() for x in self.data]
       self.input_feat_dim = 9
       self.stratification_ratios = [0.7,0.3] #hardcoded this one
-    else:
+    else: #it is a TU dataset
       data = dgl.data.TUDataset(self.name)
       self.labels = [lab.item() for lab in data.graph_labels]
       self.graphs =  data.graph_lists
       if (category == "social"):
-        #self.input_feat_dim = self.highest_degree()
         self.input_feat_dim = 1
       else:
         self.input_feat_dim = 21 if (name=="ENZYMES") else self.num_atoms()+1
@@ -119,8 +143,11 @@ class dataset():
     if (load_splits):
       self.graph_sets, self.label_sets = self.load_splits_from_json(predefined_splits_filepath)
     else:
-      self.graph_sets, self.label_sets = create_10_fold_splits(self.graphs,self.labels,random_seed=17)
-
+      if (category == 'brain'):
+        self.graph_sets, self.label_sets = create_5_fold_splits(self.graphs,self.labels,random_seed=17)
+      else:
+        self.graph_sets, self.label_sets = create_10_fold_splits(self.graphs,self.labels,random_seed=17)
+  
   def get_stratification_ratios(self,lst):
     x = OrderedDict(Counter(lst))
     ln = len(lst)
@@ -142,12 +169,6 @@ class dataset():
 
   def num_atoms(self):
     return max(max(g.ndata['node_labels']) for g in self.graphs)
-
-def split_data(datasetObj,fold_index_k=0,val_split = 0.1,validation_split_seed=17):
-  #fold_index_k = 2 #index between [0,10)
-  #gets us the desired fold as testing set
-  dataFold = dataset_split_class(datasetObj,fold_index_k=fold_index_k,val_split = 0.1,validation_split_seed=17)
-  return dataFold
 
 class dataset_split_class():
   def __init__(self,datasetObj,fold_index_k=0,val_split = 0.1,validation_split_seed=17):
@@ -213,7 +234,7 @@ def batch(graphs_list,labels,batch_size,category,num_classes,r,input_feat_dim,st
     graph_batches.append([graphs_list[index] for index in indices])
     X_batches.append([get_embed_X(graphs_list[index],category,input_feat_dim) for index in indices])
     c = get_precomputions([graphs_list[index] for index in indices],X_batches[i],r,category)
-    f = [torch.Tensor(t) for t in X_batches[i]] if (category == "social") else [get_sparse_tensor_from_scipy_coo(t) for t in X_batches[i]]
+    f = [torch.Tensor(t) for t in X_batches[i]] if (category == "social" or category == 'brain') else [get_sparse_tensor_from_scipy_coo(t) for t in X_batches[i]]
     all_batches[0].append(f)
     for j in range(1,r+1):
       all_batches[j].append(c[j-1])
@@ -225,7 +246,7 @@ def batch(graphs_list,labels,batch_size,category,num_classes,r,input_feat_dim,st
 
 def get_precomputions(graphs,X,r,category):
   all_list = [[] for _ in range(r)] #it should be like a list [AX_list,A2X_list,A3X_list,A4X_list]
-  if (category == 'social'):
+  if (category == 'social' or category == 'brain'):
     for i in range(len(X)):
       A = get_adjacency(graphs[i]) #this is the normalized adjacency
       c = []
@@ -262,7 +283,9 @@ def get_adjacency(graph): #input = DGL hetero graph
   return A_tilde
   
 def get_embed_X(graph,category,input_feat_dim):
-  if (category == 'ogb'): X = np.array(graph.ndata['feat'])
+  if (category == 'ogb'): 
+    X = np.array(graph.ndata['feat'])
+    X = coo_matrix(X)
   elif (category == 'social' or category == 'brain'): #for social and brain dataset, endcoding the degree info
     degrees = graph.in_degrees()
     X = np.array(degrees).reshape(graph.num_nodes(),1)
@@ -274,7 +297,7 @@ def get_embed_X(graph,category,input_feat_dim):
     if (input_feat_dim == 21): #special case for ENZYMES
       X1 = np.array(graph.ndata["node_attr"]) #nx18 matrix
       X = np.concatenate((X1,X[:,:3]),axis=1) #converting it to nx21
-  if (category != "social") : X = coo_matrix(X)
+    X = coo_matrix(X)
   return X
 
 def create_10_fold_splits(graphs,labels,random_seed=17):
@@ -299,7 +322,75 @@ def create_10_fold_splits(graphs,labels,random_seed=17):
 
   return graph_sets,label_sets
 
+def create_5_fold_splits(graphs,labels,random_seed=17):
+  #create the k-fold splits and save them, so that we can even use the same anytime
+  #using the unconventional train-test split method for creating 5 folds as it has the "random seed" that could help us reproduce this randomness
+  graphs_80, graphs_set_1, lab_80, lab_set_1 = train_test_split(graphs, labels, test_size=0.2, random_state=random_seed)
+
+  graphs_40_1, graphs_40_2, lab_40_1, lab_40_2 = train_test_split(graphs_80, lab_80, test_size=0.5, random_state=random_seed)
+
+  graphs_set_2,graphs_set_3,lab_set_2,lab_set_3 = train_test_split(graphs_40_1, lab_40_1, test_size=0.5, random_state=random_seed)
+  graphs_set_4,graphs_set_5,lab_set_4,lab_set_5 = train_test_split(graphs_40_2, lab_40_2, test_size=0.5, random_state=random_seed)
+
+  graph_sets = [graphs_set_1,graphs_set_2,graphs_set_3,graphs_set_4,graphs_set_5]
+  label_sets = [lab_set_1,lab_set_2,lab_set_3,lab_set_4,lab_set_5]
+
+  return graph_sets,label_sets
+
+
 def load_dataset(dataset_name,load_datasplits = False,predefined_splits_filepath = ''):
   data_dct = dataset_dict[dataset_name]
   data_obj = dataset(dataset_name,data_dct['category'],data_dct['num_classes'],load_splits = load_datasplits,predefined_splits_filepath = predefined_splits_filepath)
   return data_obj
+
+def split_data(datasetObj,fold_index_k=0,val_split = 0.1,validation_split_seed=17):
+  #fold_index_k = 2 #index between [0,10)
+  #gets us the desired fold as testing set
+  dataFold = dataset_split_class(datasetObj,fold_index_k=fold_index_k,val_split = val_split,validation_split_seed=17)
+  return dataFold
+
+#-------------utility function for constructing the brain dataset from the .nel file
+def construct_graph(lines : list) -> 'DGL graph':
+  '''
+  Given a graph represented using strings as given in format @ https://github.com/GRAND-Lab/graph_datasets, 
+  the func returns the DGL graph for it
+  '''
+  node_attr = dict()
+  src_nodes = []
+  dest_nodes = []
+  for line in lines[:-3]:
+    if (line[0]=='n'):
+      node_id, node_attr_value = re.findall(r'n\s(\d+)\sn_(\d+)',line)[0]
+      node_id = int(node_id)-1 #to start the node indexing with zero
+      node_attr[node_id] = int(node_attr_value)
+    else:
+      src_id, dest_id = re.findall(r'e\s(\d+)\s(\d+)',line)[0]
+      #fwd edge
+      src_nodes.append(int(src_id)-1)
+      dest_nodes.append(int(dest_id)-1)
+      #bck edge
+      src_nodes.append(int(dest_id)-1)
+      dest_nodes.append(int(src_id)-1)
+  graph = dgl.graph((src_nodes, dest_nodes))
+  graph.ndata['feat'] = torch.tensor([node_attr[i] for i in range(len(node_attr))])
+  #label = lines[-2][2]
+  #graph.ndata['label'] = torch.tensor([1]) if(label == '1') else torch.tensor([0])
+  return graph
+
+def construct_dataset(filename):
+  file = open(filename,"r")
+  graphs = []
+  labels = []
+  x = file.readline()
+  lines = [x]
+  while (x != ''):
+    while (x != '\n'):
+      x = file.readline()
+      lines.append(x)
+    graphs.append(construct_graph(lines))
+    label = lines[-2][2]
+    labels.append(1 if(label == '1') else 0)
+    x = file.readline()
+    lines = [x]
+  file.close()
+  return graphs,labels
