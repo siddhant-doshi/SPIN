69109992d8e91d726318786c9a5919fdb6c41fc6
Siddhant Doshi <66712952+siddhant-doshi@users.noreply.github.com>
2021-09-04 11:20:08 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,14 +1,22 @@
-
 from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.metrics import roc_auc_score, roc_curve, auc
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-def evaluate(logits,labels):
-  pred_train = [torch.argmax(logits[j]).item() for j in range(len(logits))]
-  return accuracy_score(labels,pred_train)
+sft = nn.Softmax()
 
-def train_model(dataFold,net,patience_factor,num_epochs,loss = "Cross_entropy",optimizer = "Adam",L2 = 0,learning_rate = 1e-3):
+def evaluate(logits,labels,metric = 'accuracy'):
+  if (metric == 'accuracy'):
+    pred_train = [torch.argmax(logits[j]).item() for j in range(len(logits))]
+    return accuracy_score(labels,pred_train)
+  elif (metric == 'auroc'):
+    sg = sft(logits)
+    fpr, tpr, thresholds = roc_curve(y_true = labels, y_score = sg[:,1].detach(), pos_label = 1) #positive class is 1; negative class is 0
+    auroc = auc(fpr, tpr)
+    return auroc
+
+def train_model(dataFold,net,patience_factor,num_epochs,loss = "Cross_entropy",optimizer = "Adam",L2 = 0,learning_rate = 1e-3,metric = 'accuracy'):
   avg_epochwise_loss,epochwise_val_loss,avg_epochwise_train_acc,avg_epochwise_val_acc,avg_epochwise_test_acc = [],[],[],[],[]
   best_model = {"Epoch_index":0,"model":net,"train_accuracy":0.0,"val_accuracy":0.0,"test_accuracy":0.0}
   if (loss == "Cross_entropy") : CE_loss = nn.CrossEntropyLoss()
@@ -24,23 +32,23 @@ def train_model(dataFold,net,patience_factor,num_epochs,loss = "Cross_entropy",o
       l,t_acc=0,0
       net.train()
       for i in range(dataFold.num_batches):
-        logits = net([dataFold.train_batches[j][i] for j in range(len(dataFold.train_batches[:-2]))])
+        logits = net([dataFold.train_batches[j][i] for j in range(len(dataFold.train_batches))])
         loss = CE_loss(logits,dataFold.train_batches[-2][i])
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-        b_acc = evaluate(logits,dataFold.train_batches[-2][i])
+        b_acc = evaluate(logits,dataFold.train_batches[-2][i],metric=metric)
         print ("epoch = %d | batch = %d | loss = %f | batch_accuracy = %f "%(epoch_num,i,loss.item(),b_acc))
         l+=loss.item()
         t_acc+=b_acc
       
       net.eval()
-      logits_val = net([dataFold.val_batches[j][0] for j in range(len(dataFold.val_batches[:-2]))])
+      logits_val = net([dataFold.val_batches[j][0] for j in range(len(dataFold.val_batches))])
       val_loss = CE_loss(logits_val,dataFold.val_batches[-2][0])
-      acc_val = evaluate(logits_val,dataFold.val_batches[-2][0])
+      acc_val = evaluate(logits_val,dataFold.val_batches[-2][0],metric=metric)
 
-      logits_test = net([dataFold.test_batches[j][0] for j in range(len(dataFold.test_batches[:-2]))])
-      acc_test = evaluate(logits_test,dataFold.test_batches[-2][0])
+      logits_test = net([dataFold.test_batches[j][0] for j in range(len(dataFold.test_batches))])
+      acc_test = evaluate(logits_test,dataFold.test_batches[-2][0],metric=metric)
 
       avg_epochwise_loss.append(l/dataFold.num_batches)
       epochwise_val_loss.append(val_loss)
